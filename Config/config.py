a6646a45ac5f6aba6894f160f611997bea5f68e7
JanKurdila <lektorpythonav@gmail.com>
2024-05-20 15:05:09 +0200

    3) Pohyb policajta pomocou smerových šípiek

--- Config/config.py
+++ Config/config.py
@@ -8,4 +8,6 @@ FARBA_POZADIA = pygame.Color(173, 216, 230)
 POLICAJT = pygame.image.load("IMAGES/pol.png")   
 AUTO  = pygame.image.load("IMAGES/auto.png")  
 
+STEP = 5  # Pohyb policajta o 5
+
 FPS = 25
--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import pygame
 from Config import config
 import sys
 
+def move_policeman(x_coordinate, y_coordinate, keys):
+    """Funkcia rieši pohyb policajta podľa stlačenia klávies smeru"""
+    if keys[pygame.K_LEFT]:
+        if x_coordinate > 10:  # Aby policajt nevyšiel za ľavý okraj
+            x_coordinate -= config.STEP  # Pohyb policajta vľavo
+    if keys[pygame.K_RIGHT]:
+        if x_coordinate < config.ROZLISENIE[0] - config.POLICAJT.get_width() - 10:  # Aby policajt nevyšiel za pravý okraj
+            x_coordinate += config.STEP  # Pohyb policajta vpravo
+    if keys[pygame.K_UP]:
+        if y_coordinate > 10:  # Aby policajt nevyšiel hore za okraj
+            y_coordinate -= config.STEP  # Pohyb policajta nahor
+    if keys[pygame.K_DOWN]:
+        if y_coordinate < config.ROZLISENIE[1] - config.POLICAJT.get_height() - 10:  # Aby policajt nevyšiel dole za okraj
+            y_coordinate += config.STEP  # Pohyb policajta nadol
+    return x_coordinate, y_coordinate
+
 if __name__ == "__main__":
     pygame.init()
     window = pygame.display.set_mode(config.ROZLISENIE)
@@ -9,6 +25,8 @@ if __name__ == "__main__":
 
     policajt = config.POLICAJT
     auto = config.AUTO
+
+    x, y = config.ROZLISENIE[0] // 2, config.ROZLISENIE[1] // 2 # Inicializácia pozície policajta
 
     clock = pygame.time.Clock()
 
@@ -18,10 +36,14 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit() # Vypnutie pygamu
                 sys.exit() # Vypnutie celého programu
+        
+        keys = pygame.key.get_pressed()
+        x, y = move_policeman(x, y, keys)
             
-        window.fill(config.FARBA_POZADIA) # Premazanie obrazovky
-        window.blit(policajt, (config.ROZLISENIE[0]//2, config.ROZLISENIE[1]//2))
-        window.blit(auto, (config.ROZLISENIE[0]//10, config.ROZLISENIE[1]//10))
+        window.fill(config.FARBA_POZADIA)  # Premazanie obrazovky
+
+        window.blit(policajt, (x, y))
+        window.blit(auto, (config.ROZLISENIE[0] // 10, config.ROZLISENIE[1] // 10))
 
         pygame.display.update()
 
