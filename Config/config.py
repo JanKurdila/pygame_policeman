7cd8d8cc20b599e22a9366b638fa03969d012bca
JanKurdila <lektorpythonav@gmail.com>
2024-05-20 14:57:23 +0200

    2) Vygenerovanie a vykreslenie policajta a auta

--- Config/config.py
+++ Config/config.py
@@ -1,6 +1,11 @@
 import pygame
 
-ROZLISENIE = (900, 500)
+ROZLISENIE = (1100, 600)
 
 # Farby
-FARBA_POZADIA = pygame.Color(173, 216 , 230)
+FARBA_POZADIA = pygame.Color(173, 216, 230)
+
+POLICAJT = pygame.image.load("IMAGES/pol.png")   
+AUTO  = pygame.image.load("IMAGES/auto.png")  
+
+FPS = 25
--- main.py
+++ main.py
@@ -7,11 +7,23 @@ if __name__ == "__main__":
     window = pygame.display.set_mode(config.ROZLISENIE)
     pygame.display.set_caption("Policajt verzus auta")
 
+    policajt = config.POLICAJT
+    auto = config.AUTO
+
+    clock = pygame.time.Clock()
+
     while True:
         # Ak vypnem okno, musím vypnuť pygame
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit() # Vypnutie pygamu
                 sys.exit() # Vypnutie celého programu
+            
+        window.fill(config.FARBA_POZADIA) # Premazanie obrazovky
+        window.blit(policajt, (config.ROZLISENIE[0]//2, config.ROZLISENIE[1]//2))
+        window.blit(auto, (config.ROZLISENIE[0]//10, config.ROZLISENIE[1]//10))
 
         pygame.display.update()
+
+        # Spomalenie cyklu
+        clock.tick(config.FPS) # Obnova hada - resp.obrázkov
